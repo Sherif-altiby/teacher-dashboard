bc8e2a3f44d49f452f413028da4cf4a662279642
Sherif Altiby <sherifahmed852002@gmail.com>
2026-04-15 05:00:08 +0200

    upate Next.Config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    // يسمح بجلب الصور من أي رابط عبر الـ Wildcard (**)
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**', // This is the wildcard that accepts everything
+        protocol: "https",
+        hostname: "**", 
       },
       {
-        protocol: 'http',
-        hostname: '**',
+        protocol: "http",
+        hostname: "**",
       },
     ],
+  },
+
+  // كود الـ Rewrites لحل مشكلة الكوكيز (Proxy)
+  async rewrites() {
+    return [
+      {
+        // سيقوم هذا الكود بتوجيه أي طلب يبدأ بـ /api-backend إلى سيرفر الباك إند الحقيقي
+        source: '/api-backend/:path*',
+        destination: 'https://platform-gamma-one.vercel.app/api/:path*', 
+      },
+    ];
   },
 };
 
