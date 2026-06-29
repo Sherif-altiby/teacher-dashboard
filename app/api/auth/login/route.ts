28ffcf760525a5a615f9a81de178214d4cafda81
Sherif Altiby <sherifahmed852002@gmail.com>
2026-06-29 21:13:59 +0300

    update login end point

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 export async function POST(request: Request) {
   const body = await request.json();
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/teacher/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
